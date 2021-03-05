2f088a6c3d63e5bb11a7101bade102880491d193
objectiphy <rwalker.php@gmail.com>
2021-03-05 11:46:45 +0000

    wip:getting ready for serialization group annotations

---
+++ tests/Entity/TestNormalEntity.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Objectiphy\Annotations\Tests\Annotations\Column;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+class TestNormalEntity
+{
+    /**
+     * @Column(name="product", type="string")
+     * @Groups({"Default"})
+     */
+    protected string $product;
+}
